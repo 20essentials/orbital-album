904c319860dfb5d6dc412e9dd44db3c4a4dd0dc6
DominicDev <s0l0estudi4r@gmail.com>
2026-02-18 18:16:11 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ORBITAL';
+export let nameSongs = [
+	"CHIME", /*1*/
+	"BELFAST", /*2*/
+	"HALCYON AND ON AND ON", /*3*/
+	"REMIND", /*4*/
+	"MONDAY", /*5*/
+	"LUSH 3-1", /*6*/
+	"PLANET OF THE SHAPES", /*7*/
+	"DWR BUDR", /*8*/
+	"DOCTOR?", /*9*/
+	"TINY FOLDABLE CITIES", /*10*/
+	"YOU LOT", /*11*/
+	"ONE BIG MOMENT", /*12*/
+	"WAY OUT", /*13*/
+	"THE NEW ABNORMAL", /*14*/
+	"REQUIEM FOR THE PRE-APOCALYPSE", /*15*/
+	"RINGA RINGA", /*16*/
+	"HALCYON", /*17*/
+	"GIRL WITH THE SUN IN HER HEAD", /*18*/
+	"ARE WE HERE?", /*19*/
+	"WHERE IS IT GOING?", /*20*/
+]
